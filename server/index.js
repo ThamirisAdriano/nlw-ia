f16a13ed5aeeaac07cb7deb8329b4103817b6fa0
Thamiris Adriano <adrianosthamiris@gmail.com>
2023-09-13 12:24:25 -0300

    aula back part 2

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 import cors from "cors"
 import express from "express"
 
+import { download } from "./download.js"
+
 const app = express()
-app.use(cors()) //habilita a conexão do back com o front
+app.use(cors()) 
 
 //criando a rota
 app.get("/summary/:id", (request, response) => {
-  response.send("ID do vídeo: " + request.params.id) //queremos recuperar de dentro da requisição o id do parametro
-}) //quando a requisição for feita para o servidor utilizando get, ele executa a função
-
-app.listen(3333, () => console.log("Server is running on port 3333")) //método para escutar as requisições (porta, msg)
+  download(request.params.id)
+  response.json({ result: "Download do vídeo realizado com sucesso!" })
+})
+app.listen(3333, () => console.log("Server is running on port 3333"))
